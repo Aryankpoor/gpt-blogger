8bcd8ce0f4ffca7f116f52e73c40f5493cbf1448
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-08 14:40:22 +0530

    updated port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = process.env.port || 4000;
+const port = "https://sec-backend-1.onrender.com/";
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
